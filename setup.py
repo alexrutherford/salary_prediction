28c6de0a3a9ec85933c3f2c9be3e02d4b24d84fb
Alex Rutherford <alexisadams@gmail.com>
2022-04-29 13:01:33 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A prototype prediction model to predict salary of a job based on the description',
+    author='Alex Rutherford',
+    license='MIT',
+)
